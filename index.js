da6da289dd1b6da3bcb68c4c9a0b0ab1a65d56b7
Leonel <leonelrcordero22@gmail.com>
2024-03-04 10:26:11 -0300

    Agregamos los comandos start y dev para ejecutar el repo de backend en produccion y desarrollo

---
+++ index.js
@@ -0,0 +1,5 @@
+import express from 'express';
+console.log('hola mundo v3');
+
+//1- Configurar un puerto
+// puedo compilar el index.js de manera constante usando node --watch en forma experimental
