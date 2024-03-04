077b4667b6aadaa6cc4a90405638586c181ba927
Leonel <leonelrcordero22@gmail.com>
2024-03-04 10:54:20 -0300

    Tomamos el puerto de una variable de entorno o en caso contrario uso mi puerto 4000

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.log('hola mundo v3');
 
 //1- Configurar un puerto
 // puedo compilar el index.js de manera constante usando node --watch en forma experimental
+const app = express();
+//Crear una variable con express
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), () => {
+  console.log('Estoy en el puerto ' + app.get('port'));
+});
+
+//2- Configurar los middleware del proyecto
+
+//3- Configurar las rutas
