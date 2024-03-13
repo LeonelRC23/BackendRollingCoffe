553ba35c2bfc83aedb95943a925d6b10c3e2842b
Leonel <leonelrcordero22@gmail.com>
2024-03-13 13:47:17 -0300

    Se realizo la logica de borrado

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -4,11 +4,16 @@ import {
   crearProducto,
   obtenerProducto,
   editarProducto,
+  borrarProducto,
 } from '../controllers/productos.controllers.js';
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto);
-router.route('/producto/:id').get(obtenerProducto).put(editarProducto);
+router
+  .route('/producto/:id')
+  .get(obtenerProducto)
+  .put(editarProducto)
+  .delete(borrarProducto);
 
 export default router;
