091c0ce72635ba984faa2c29ad7d049cc58c3573
Leonel <leonelrcordero22@gmail.com>
2024-03-13 10:32:48 -0300

    Agregamos la funcion para obtener el producto por su ID

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import {
   listarProductos,
   crearProducto,
+  obtenerProducto,
 } from '../controllers/productos.controllers.js';
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto);
+router.route('/producto/:id').get(obtenerProducto);
 
 export default router;
