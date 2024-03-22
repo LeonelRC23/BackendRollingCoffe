69a817977a62f56b4d284e387fc80c51827ca20b
Leonel <leonelrcordero22@gmail.com>
2024-03-22 10:30:54 -0300

    Se termino de realizar las validaciones

--- src/helpers/validacionProductos.js
+++ src/helpers/validacionProductos.js
@@ -19,6 +19,30 @@ const validacionesProducto = [
         throw new Error('El precio debe estar entre $50 y $10000');
       }
     }),
+  check('imagen')
+    .notEmpty()
+    .withMessage('La imagen es un dato obligatorio')
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+    .withMessage(
+      'La imagen debe tener un formato de URL valida y terminar en jpg|jpeg|gif|png'
+    ),
+  check('categoria')
+    .notEmpty()
+    .withMessage('La categoria es obligatoria')
+    .isIn(['Infusiones', 'Batidos', 'dulce', 'salado'])
+    .withMessage(
+      "La categoria debe ser una de las siguientes opciones: 'Infusiones', 'Batidos', 'dulce', 'salado'"
+    ),
+    check('descripcionBreve')
+    .notEmpty()
+    .withMessage('La descripcion del producto es un dato obligatorio')
+    .isLength({ min: 5, max: 50 })
+    .withMessage('La descripcion debe tener entre 2 y 50 catacteres'),
+    check('descripcionAmplia')
+    .notEmpty()
+    .withMessage('La descripcion del producto es un dato obligatorio')
+    .isLength({ min: 2, max: 50 })
+    .withMessage('La descripcion del producto debe tener entre 2 y 50 catacteres'),
   (req, res, next) => {
     resultadoValidacion(req, res, next);
   },
