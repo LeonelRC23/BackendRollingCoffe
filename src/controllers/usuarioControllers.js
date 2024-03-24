07391f0841b211f1baf862af36a850b1de1d6161
Leonel <leonelrcordero22@gmail.com>
2024-03-23 21:41:36 -0300

    Agregada validaciones para el correo con expresiones regulares

--- src/controllers/usuarioControllers.js
+++ src/controllers/usuarioControllers.js
@@ -3,23 +3,31 @@ import bcrypts from 'bcryptjs';
 
 export const register = async (req, res) => {
   try {
+    const expCorreo =
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
     const { email, password, role } = req.body;
-    const emailValidacion = await usuarioModel.findOne({ email: email });
-    if (emailValidacion) {
-      res.status(200).json({
-        mensaje: 'Este correo ya se encuentra registrado.',
-      });
+    if (expCorreo.test(email)) {
+      const emailValidacion = await usuarioModel.findOne({ email: email });
+      if (emailValidacion) {
+        res.status(200).json({
+          mensaje: 'Este correo ya se encuentra registrado.',
+        });
+      } else {
+        const saltos = await bcrypts.genSalt(8);
+        const passEncriptada = await bcrypts.hash(password, saltos);
+        const crearUsuario = new usuarioModel({
+          email: email,
+          password: passEncriptada,
+          role: role,
+        });
+        crearUsuario.save();
+        res.status(200).json({
+          mensaje: 'Usuario creado correctamente.',
+        });
+      }
     } else {
-      const saltos = await bcrypts.genSalt(8);
-      const passEncriptada = await bcrypts.hash(password, saltos);
-      const crearUsuario = new usuarioModel({
-        email: email,
-        password: passEncriptada,
-        role: role,
-      });
-      crearUsuario.save();
-      res.status(200).json({
-        mensaje: 'Usuario creado correctamente.',
+      return res.status(400).json({
+        mensaje: 'Correo invalido.',
       });
     }
   } catch (error) {
